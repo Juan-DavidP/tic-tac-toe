0baca43af1c1c5e2c4efe1673915eb4ccbfc0bb1
Juan-DavidP <juandavidpelaezvilla@gmail.com>
2025-06-16 20:42:24 -0500

    Estructuración básica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./components/container/container.component";
+import { GameInfoComponent } from './components/game-info/game-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent, GameInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
